a288f61a970a03529479e64f448041a2f7a455b1
Aleksandr <72134053+Aleksandr-coder@users.noreply.github.com>
2021-01-12 18:40:40 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,6 +11,18 @@ $(document).ready(function() {
 		$("html, body").animate({scrollTop: 0}, 900)
 		return false;
 	});
+	//плавный переход от ссылки до якоря
+	$('a[href^="#"]').on("click", function (event) {
+	        //отменяем стандартную обработку нажатия по ссылке
+	        event.preventDefault();
+	        //забираем идентификатор блока с атрибута href
+	        var id  = $(this).attr('href'),
+	        //узнаем высоту от начала страницы до блока на который ссылается якорь
+	         	top = $(id).offset().top;
+	        //анимируем переход на расстояние - top за 1500 мс
+	        $('html, body').animate({scrollTop: top}, 1500);
+	    });
+	
 });
 
 
--- js/togler_click.js
+++ js/togler_click.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('.navbar-toggler').click(function(event){
-		$('.content_header').toggleClass('active');
+	$('.menu_bar, .menu_link').click(function(event){
+		$('.menu_top, .fa-bars, .fa-times, .nav_top, .content_header, .nav, body').toggleClass('active_menu_bar');
 	});
 });
